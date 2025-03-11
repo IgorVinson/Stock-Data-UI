f2c1630925d139be6fdc3272d50b33ba31a304db
Igor T <igorvinson@gmail.com>
2025-03-11 19:17:56 -0400

    fix: reinstall dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [],
-    },
-  },
-  ssr: {
-    noExternal: [
-      '@mui/material',
-      '@mui/utils',
-      '@mui/base',
-      '@mui/icons-material',
-    ],
-  },
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  optimizeDeps: {
+    include: ['@mui/material'],
   },
 });
